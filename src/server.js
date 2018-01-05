9c09f88f8473d96c268889a28398d5192ab397ca
Zsolt Szilagyi <kameli-2@iki.fi>
2018-01-05 21:31:37 +0200

    Use process.env.PORT as port if exists

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 });
 
 // Start server
-let server = app.listen(1337, function () {
+let server = app.listen(process.env.PORT || 1337, function () {
   let host = server.address().address;
   let port = server.address().port;
 
